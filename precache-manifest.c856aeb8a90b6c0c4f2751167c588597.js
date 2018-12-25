2df2b8a819c6a91b1335f1461a986023419425f0
Anna Kowalczuk <anna@kowalczuk.eu>
2018-12-25 16:59:56 +0100

    Updates

---
+++ precache-manifest.c856aeb8a90b6c0c4f2751167c588597.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a50dd2bde11df087a32d",
+    "url": "/rascia-react-tutorial/static/js/runtime~main.a50dd2bd.js"
+  },
+  {
+    "revision": "739fcbbb4edec371d579",
+    "url": "/rascia-react-tutorial/static/js/main.739fcbbb.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/rascia-react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "739fcbbb4edec371d579",
+    "url": "/rascia-react-tutorial/static/css/main.b7e9315e.chunk.css"
+  },
+  {
+    "revision": "0d3c7ba1c7e50be226b85ebf750fae02",
+    "url": "/rascia-react-tutorial/index.html"
+  }
+];
